14187f9f3eb710e5618b3aba711eebd02f563345
Ambaram  Srivastava <ambaramsrivastava@gmail.com>
2021-08-20 20:10:47 +0530

    updates

--- main.v1.js
+++ main.v1.js
@@ -4,6 +4,13 @@ var server = http.createServer(function(req,res){
 res.setHeader('Content-Security-Policy',"script-src 'self'")
 });
 
+
+function requestHandler(req, res) {
+  res.setHeader('X-XSS-Protection','1;mode=block');
+}
+
+Response.setHeader("X-Frame-Options: DENY");
+
 document.getElementById("heading").innerHTML = 
 `Vite and Web Dev
 `
