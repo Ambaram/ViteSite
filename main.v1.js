3472883d0eb4401639be2541cbd298efb458d0ce
Ambaram  Srivastava <ambaramsrivastava@gmail.com>
2021-08-20 21:20:23 +0530

    updates

--- main.v1.js
+++ main.v1.js
@@ -3,3 +3,10 @@ import './style.v1.scss'
 document.getElementById("heading").innerHTML = 
 `Vite and Web Dev
 `
+
+document.getElementById("submit").onclick = alertpop;
+
+function alertpop(){
+    const pass = document.getElementById("exampleInputPassword1").innerHTML;
+    window.alert("Your data is safe with us.");
+}
