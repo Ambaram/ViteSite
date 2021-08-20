6c08154dbdbaf2b80bde353dd28c573e3dcf8528
Ambaram  Srivastava <ambaramsrivastava@gmail.com>
2021-08-20 19:40:50 +0530

    updates

--- main.v1.js
+++ main.v1.js
@@ -1,5 +1,9 @@
 import './style.v1.scss'
 
+var server = http.createServer(function(req,res){
+res.setHeader('Content-Security-Policy',"script-src 'self'")
+});
+
 document.getElementById("heading").innerHTML = 
 `Vite and Web Dev
 `
