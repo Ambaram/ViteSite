e55cb967f12f1bfbaae0780fc9e8d77ec8b03c03
Ambaram  Srivastava <ambaramsrivastava@gmail.com>
2021-08-20 20:36:26 +0530

    updates

--- main.v1.js
+++ main.v1.js
@@ -1,16 +1,5 @@
 import './style.v1.scss'
 
-var http = require('http');
-// Setting up PORT
-const PORT = process.env.PORT || 5500;
-  
-// Creating http Server
-var httpServer = http.createServer(
-       function(request, response) {
-  
-  // Setting up Headers
-  response.setHeader('X-Frame-Options', 'DENY')});
-
 document.getElementById("heading").innerHTML = 
 `Vite and Web Dev
 `
