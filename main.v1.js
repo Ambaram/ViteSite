f760e6302c771cfdacd3cee198239a496afbf663
Ambaram  Srivastava <ambaramsrivastava@gmail.com>
2021-08-20 20:33:01 +0530

    updates

--- main.v1.js
+++ main.v1.js
@@ -1,6 +1,15 @@
 import './style.v1.scss'
 
-Response.setHeader("X-Frame-Options: DENY");
+var http = require('http');
+// Setting up PORT
+const PORT = process.env.PORT || 5500;
+  
+// Creating http Server
+var httpServer = http.createServer(
+       function(request, response) {
+  
+  // Setting up Headers
+  response.setHeader('X-Frame-Options', 'DENY')});
 
 document.getElementById("heading").innerHTML = 
 `Vite and Web Dev
