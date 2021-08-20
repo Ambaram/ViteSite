fe99d588ab5491919a127f3750a83087d0a01ea3
Ambaram  Srivastava <ambaramsrivastava@gmail.com>
2021-08-20 20:26:39 +0530

    updates

--- main.v1.js
+++ main.v1.js
@@ -1,14 +1,5 @@
 import './style.v1.scss'
 
-var server = http.createServer(function(req,res){
-res.setHeader('Content-Security-Policy',"script-src 'self'")
-});
-
-
-function requestHandler(req, res) {
-  res.setHeader('X-XSS-Protection','1;mode=block');
-}
-
 Response.setHeader("X-Frame-Options: DENY");
 
 document.getElementById("heading").innerHTML = 
